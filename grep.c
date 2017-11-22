c6080fd8f6af998d0ded2184c7d8946f70a63c09
Henry Chen <tahafut@gmail.com>
2017-11-22 12:33:38 -0800

    pattern from command line

--- grep.c
+++ grep.c
@@ -1,18 +1,20 @@
 #include "grep.h"
 #include <stdio.h>
+#include <string.h>
 
 #define MAXLINE 1000
 
-char pattern[] = "ould";
-
-int main(void) {
+int main(int argc, char *argv[]) {
   char line[MAXLINE];
   int found = 0;
 
-  while (get_line(line, MAXLINE) > 0)
-    if (strindex(line, pattern) >= 0) {
-      printf("%s", line);
-      found++;
-    }
+  if (argc != 2)
+    printf("Usage: find pattern\n");
+  else
+    while (get_line(line, MAXLINE) > 0)
+      if (strstr(line, argv[1]) != NULL) {
+        printf("%s", line);
+        found++;
+      }
   return found;
 }
