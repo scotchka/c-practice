0dd6c3e8fb60130bf2a225b84623bbd88e5a5ae0
Henry Chen <tahafut@gmail.com>
2017-11-22 11:22:34 -0800

    simplify

--- factorial.c
+++ factorial.c
@@ -3,16 +3,21 @@
 
 int main(int argc, char *argv[]) {
 
+  static int acc = 1;
+
+  if (argc < 2) {
+    printf("error: need argument\n");
+    return 0;
+  }
+
   int n = atoi(argv[1]);
-  int acc = atoi(argv[2]);
-  if (n == 1) {
+
+  if (n <= 1) {
     printf("%d\n", acc);
     return 1;
   } else {
     acc = n * acc;
-    --n;
-    sprintf(argv[1], "%d", n);
-    sprintf(argv[2], "%d", acc);
+    sprintf(argv[1], "%d", --n);
     return main(argc, argv);
   }
 }
