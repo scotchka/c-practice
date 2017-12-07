aab06c1cbeeedb684eded7a57ab6ca8fff0ff600
Henry Chen <tahafut@gmail.com>
2017-12-07 14:35:57 -0800

    split off getword

---
+++ bst.c
@@ -0,0 +1,8 @@
+struct tnode {
+  char *word;
+  int count;
+  struct tnode left;
+  struct tnode right;
+};
+
+int main(void){}
--- keyarray.c
+++ keyarray.c
@@ -54,28 +54,3 @@ struct key *binsearch(char *word, struct key *tab, int n) {
   }
   return NULL;
 }
-
-int getword(char *word, int lim) {
-  int c, getch(void);
-  void ungetch(int);
-  char *w = word;
-
-  while (isspace(c = getch()))
-    ;
-
-  if (c != EOF)
-    *w++ = c;
-
-  if (!isalpha(c)) {
-    *w = '\0';
-    return c;
-  }
-
-  for (; --lim > 0; ++w)
-    if (!isalnum(*w = getch())) {
-      ungetch(*w);
-      break;
-    }
-  *w = '\0';
-  return word[0];
-}
