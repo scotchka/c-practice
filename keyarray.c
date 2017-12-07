01c6fa31cc896d14b4f98d1e024b5915b4836a29
Henry Chen <tahafut@gmail.com>
2017-12-07 13:01:28 -0800

    use pointers

--- keyarray.c
+++ keyarray.c
@@ -21,40 +21,38 @@ struct key keytab[] = {
 };
 
 int getword(char *, int);
-int binsearch(char *, struct key *, int);
+struct key *binsearch(char *, struct key *, int);
 
 int main(void) {
-  int n;
+  struct key *p;
   char word[MAXWORD];
 
   while (getword(word, MAXWORD) != EOF)
     if (isalpha(word[0]))
-      if ((n = binsearch(word, keytab, NKEYS)) >= 0) {
-        printf("%s\n", word);
-        keytab[n].count++;
+      if ((p = binsearch(word, keytab, NKEYS)) != NULL) {
+        p->count++;
       }
 
-  for (n = 0; n < NKEYS; ++n)
-    if (keytab[n].count > 0)
-      printf("%4d %s\n", keytab[n].count, keytab[n].word);
+  for (p = keytab; p < keytab + NKEYS; ++p)
+    if (p->count > 0)
+      printf("%4d %s\n", p->count, p->word);
   return 0;
 }
 
-int binsearch(char *word, struct key tab[], int n) {
-  int cond, low, high, mid;
+struct key *binsearch(char *word, struct key *tab, int n) {
+  int cond;
+  struct key *low = &tab[0], *high = &tab[n], *mid;
 
-  low = 0;
-  high = n - 1;
-  while (low <= high) {
-    mid = (low + high) / 2;
-    if ((cond = strcmp(word, tab[mid].word)) < 0)
-      high = mid - 1;
+  while (low < high) {
+    mid = low + (high - low) / 2;
+    if ((cond = strcmp(word, mid->word)) < 0)
+      high = mid;
     else if (cond > 0)
       low = mid + 1;
     else
       return mid;
   }
-  return -1;
+  return NULL;
 }
 
 int getword(char *word, int lim) {
